0a760f7b7d0b9be329878e81a476f4821195a5b2
Jordan Myers <jordan.v.myers@gmail.com>
2016-02-20 20:31:18 -0600

    cleaned up save_map

--- getMap.py
+++ getMap.py
@@ -1,23 +1,16 @@
 import urllib
+import Image
 
-'''
-import json
 
-query = '' # IP to get coordinates of, leave empty for current IP
-
-geo = urllib.urlopen('http://ip-api.com/json/%s?fields=240' % query)
-print geo
-help(geo)
-result = json.load(geo)
-if result['zip']:
-    zoom = 13
-elif result['city']:
-    zoom = 12
-else:
-    zoom = 6
-map_img_url = "https://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zoom=%i&size=320x385&sensor=false" % (result['lat'], result['lon'], zoom)
-get_map_img = urllib.urlretrieve(map_img_url, "staticmap.png")
-'''
-def save_map(lat, lon, filename='map.png', zoom=6):
+def save_map(lat, lon, filename='map.png', zoom=20, show=True):
 	map_img_url = "https://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zoom=%i&size=320x385&sensor=false" % (lat, lon, zoom)
-	urllib.urlretrieve(map_img_url, filename)
+	urllib.urlr]etrieve(map_img_url, filename)
+		
+if __name__ == '__main__':  #sample script that gets map of location at current IP address
+	import json
+	query = '' # IP to get coordinates of, leave empty for current IP
+	geo = urllib.urlopen('http://ip-api.com/json/%s?fields=240' % query)
+	result = json.load(geo)
+	save_map(result['lat'], result['lon'])
+	image = Image.open(filename)
+	image.show()
