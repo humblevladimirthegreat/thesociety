3a26d2c07557844d0f0cc0e797707caa092e8d67
humblevladimirthegreat <humblevladimirthegreat@gmx.com>
2016-02-21 15:12:20 -0600

    fix to allow float values for lat,lon

--- CropCircles.py
+++ CropCircles.py
@@ -3,7 +3,7 @@ import subprocess
 
 if __name__ == '__main__':
     #use 40, -88
-    lat = int(input("Input latitude coordinate:"))
-    lon = int(input("Input longitude coordinate:"))
+    lat = float(input("Input latitude coordinate:"))
+    lon = float(input("Input longitude coordinate:"))
     save_map(lat, lon)
     subprocess.run(['java', '-cp', '.', 'AddCropCircles', 'map.png'])
