dcb5ff7fc1a9a4a2d32cd9e18023c08582f4c291
humblevladimirthegreat <humblevladimirthegreat@gmx.com>
2016-02-21 14:58:48 -0600

    added lat,lon input to cropcircles, tried to fix datagetter

--- CropCircles.py
+++ CropCircles.py
@@ -2,5 +2,8 @@ from getMap import save_map
 import subprocess
 
 if __name__ == '__main__':
-    save_map(40, -88)
+    #use 40, -88
+    lat = int(input("Input latitude coordinate:"))
+    lon = int(input("Input longitude coordinate:"))
+    save_map(lat, lon)
     subprocess.run(['java', '-cp', '.', 'AddCropCircles', 'map.png'])
--- DataGetter.py
+++ DataGetter.py
@@ -3,7 +3,8 @@ import json
 
 # User ID: 151069
 # Authorization Token: 7a43a937-6f23-4e2a-8d52-d5f1f65b1639
-
+auth_token = '7a43a937-6f23-4e2a-8d52-d5f1f65b1639'
+hard_id = 151069
 
 def getFieldData(authorization):
     authstring = 'Bearer ' + authorization
@@ -19,6 +20,39 @@ def getFieldData(authorization):
     connection.close()
     return json.loads(data)
 
+def getFieldDataById(authorization, user_id):
+    authstring = 'Bearer ' + authorization
+    connection = HTTPSConnection('hackillinois.climate.com')
+    headers = {'Authorization': authstring,
+               'Content-type': 'application/json',
+               'Accept': 'application/json'}
+
+    connection.request('GET', '/api/fields?includeBoundary=true&userId=%s'%user_id, headers=headers)
+    response = connection.getresponse()
+    data = response.read().decode()
+    print(data)
+    connection.close()
+    return json.loads(data)
+    
+    
+def getUserId(authorization, email):
+    authstring = 'Bearer ' + authorization
+    connection = HTTPSConnection('hackillinois.climate.com')
+    headers = {'Authorization': authstring,
+               'Content-type': 'application/json',
+               'Accept': 'application/json'}
+    #email = email.replace("@", 
+    connection.request('GET', '/users/details?email='+email, headers=headers)
+    response = connection.getresponse()
+    data = response.read().decode()
+    print(data)
+    connection.close()
+    the_json = json.loads(data)
+    if 'id' in the_json:
+        return the_json['id']
+    else:
+        return 0
+
 
 def saveFieldData(fielddata, filename):
     text = json.dumps(fielddata)
@@ -28,6 +62,11 @@ def saveFieldData(fielddata, filename):
 
 def getCoordinates(fieldData):
     return fieldData['coords']
+
+#user_id = getUserId(auth_token, 'humblevladimirthegreat@gmx.com')
+#print ('user_id = %s'%user_id)
+#fieldData = getFieldDataById(auth_token, hard_id)
+#print(fieldData)
 
 fieldData = getFieldData('7a43a937-6f23-4e2a-8d52-d5f1f65b1639')
 print(fieldData)
